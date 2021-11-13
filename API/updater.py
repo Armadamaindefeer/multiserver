1319e82d60fe8df26fe4526a92e34ab1388da05d
Armadamaindefeer <74682524+Armadamaindefeer@users.noreply.github.com>
2021-11-13 04:10:18 +0100

    Update updater.py

--- API/updater.py
+++ API/updater.py
@@ -3,6 +3,7 @@
 import json
 import os
 import requests
+import sublibrary.constant
 
 #>>-----------Function--------------<<
 
@@ -11,32 +12,62 @@ def getNameFromGithub(url):
 		return (url.rsplit('/', 1)[1])
 
 
-def getLibManifest():
+def getLibManifest(workingDir):
 	print('Downloading manifest')
 	urlManifest = 'https://raw.githubusercontent.com/Armadamaindefeer/multiserver/main/manifest.json'
 	r = requests.get(urlManifest)
-	manifest = open(('.intern/.temp/' + getNameFromGithub(urlManifest)), 'wb').write(r.content)
-
+	manifest = open((workingDir + getNameFromGithub(urlManifest)), 'wb').write(r.content)
+	with open(workingDir + getNameFromGithub(urlManifest), 'r') as manifest:
+		libManifest = json.load(manifest)
 	return libManifest
 
+def downloadLib(url, name, workingDir)
+	r = requests.get(url)
+	if not open((workingDir + '/' + name), 'wb').write(r.content)
+		return False
+	else:
+		return True
+
+
+
+#>>-----------Main------------------<<
 
 def checkLib():
-	tempLibManifest = getLibManifest()
-	with open('.intern/.temp/manifest.json', 'r') as manifest:
-		libManifest = json.load(manifest)
+
 	countLib = 0
 	countTempLib = 0
-	for key in tempLibManifest['lib']:
-		countTempLib += 1
-		for keyb in libManifest['lib']:
-			countLib += 1
-			if key['name'] == keyb['name'] and key['version'] != keyb['version'] :
-				r = requests.get(key['download'])
-				if not open((internDir + '.lib/' + key['name']), 'wb').write(r.content):
-					print('Error while updating')
+	with open(LIBRARY_DIR + '/manifest.json', 'r') as manifest:
+		libManifest = json.load(manifest)
 
-	countLib = countLib/countTempLib
-	if countLib != countTempLib:
-		print('Error while updating')
-def main():
-	return 0
+	for key in tempLibManifest['library']:
+
+		countTwoLib = 0
+		for kex in libManifest['library']:
+#Update of library
+			if key['name'] == kex['name'] and key['version'] != kex['version']:
+				if 'subfolder' in key and key['subfolder'] == True:
+					if not downloadLib(key['download'], key['name'], SUB_LIBRARY_DIR):
+						print('Error while updating')
+					else:
+						countTwoLib += 1
+						changeManifest = True
+				else:
+					if not downloadLib(key['download'], key['name'], LIBRARY_DIR):
+						print('Error while updating')
+					else:
+						countTwoLib += 1
+						changeManifest = True
+#Downloading of new library
+		if countTwoLib <= 0 and key['required']:
+			if 'subfolder' in key and key['subfolder'] == True:
+				if not downloadLib(key['download'], key['name'], SUB_LIBRARY_DIR):
+					print('Error while updating')
+				else:
+					changeManifest = True
+			else:
+				if not downloadLib(key['download'], key['name'], LIBRARY_DIR):
+					print('Error while updating')
+				else:
+					changeManifest = True
+	if  changeManifest:
+		os.replace(TEMP_DIR + '/manifest.json',  LIBRARY_DIR + '/manifest.json')
