c302f9b161498af2b06e6be6949e7a884044851b
Armadamaindefeer <74682524+Armadamaindefeer@users.noreply.github.com>
2021-11-13 17:16:53 +0100

    Update MultiServer.py

--- MultiServer.py
+++ MultiServer.py
@@ -6,13 +6,13 @@ import os
 import cmd
 import requests
 import colorama
-import data.library.utility
-import data.library.updater
-import data.library.sublibrary.constant
+import data.library.utility as utility
+import data.library.updater as updater
+from data.library.constant import *
 
 def main():
 	updater.checkLib()
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
 	main()
