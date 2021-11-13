b19d4946539ada2c118d18ae012d0bd0305f0331
Armadamaindefeer <74682524+Armadamaindefeer@users.noreply.github.com>
2021-11-13 17:13:44 +0100

    Update updater.py

--- API/updater.py
+++ API/updater.py
@@ -3,7 +3,7 @@
 import json
 import os
 import requests
-import sublibrary.constant
+from data.library.constant import *
 
 #>>-----------Function--------------<<
 
@@ -23,7 +23,7 @@ def getLibManifest(workingDir):
 
 def downloadLib(url, name, workingDir):
 	r = requests.get(url)
-	if not open((workingDir + '/' + name), 'wb').write(r.content)
+	if not open((workingDir + '/' + name), 'wb').write(r.content):
 		return False
 	else:
 		return True
@@ -34,40 +34,24 @@ def downloadLib(url, name, workingDir):
 
 def checkLib():
 
-	countLib = 0
-	countTempLib = 0
+	tempLibManifest = getLibManifest(TEMP_DIR)
 	with open(LIBRARY_DIR + '/manifest.json', 'r') as manifest:
 		libManifest = json.load(manifest)
-
 	for key in tempLibManifest['library']:
-
 		countTwoLib = 0
 		for kex in libManifest['library']:
 #Update of library
 			if key['name'] == kex['name'] and key['version'] != kex['version']:
-				if 'subfolder' in key and key['subfolder'] == True:
-					if not downloadLib(key['download'], key['name'], SUB_LIBRARY_DIR):
-						print('Error while updating')
-					else:
-						countTwoLib += 1
-						changeManifest = True
-				else:
-					if not downloadLib(key['download'], key['name'], LIBRARY_DIR):
-						print('Error while updating')
-					else:
-						countTwoLib += 1
-						changeManifest = True
-#Downloading of new library
-		if countTwoLib <= 0 and key['required']:
-			if 'subfolder' in key and key['subfolder'] == True:
-				if not downloadLib(key['download'], key['name'], SUB_LIBRARY_DIR):
-					print('Error while updating')
-				else:
-					changeManifest = True
-			else:
 				if not downloadLib(key['download'], key['name'], LIBRARY_DIR):
 					print('Error while updating')
 				else:
+					countTwoLib += 1
 					changeManifest = True
+#Downloading of new library
+		if countTwoLib <= 0 and key['required']:
+			if not downloadLib(key['download'], key['name'], LIBRARY_DIR):
+				print('Error while updating')
+			else:
+				changeManifest = True
 	if  changeManifest:
 		os.replace(TEMP_DIR + '/manifest.json',  LIBRARY_DIR + '/manifest.json')
