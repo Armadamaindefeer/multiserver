8db6e9e1868cd0df91d06785e38eabd11152c3fc
Armadamaindefeer <74682524+Armadamaindefeer@users.noreply.github.com>
2021-11-13 04:08:37 +0100

    Update constant.py

--- API/constant.py
+++ API/constant.py
@@ -5,7 +5,8 @@ ERROR = 2
 CRITICAL = 3
 
 DATA_DIR = 'data'
-CONFIG_DIR = 'config'
-LIBRARY_DIR = 'library'
-TEMP_DIR = '.temp'
-SERVER_DIR = 'servers'
+CONFIG_DIR = DATA_DIR + '/config'
+LIBRARY_DIR = DATA_DIR + '/library'
+SUB_LIBRARY_DIR = LIBRARY_DIR + '/sublibrary'
+TEMP_DIR = DATA_DIR + '/.temp'
+SERVER_DIR = DATA_DIR + '/servers'
