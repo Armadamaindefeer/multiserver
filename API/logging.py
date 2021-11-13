69cb0d88716e5f98cb826cbb68f47c0be732a5d5
Armadamaindefeer <74682524+Armadamaindefeer@users.noreply.github.com>
2021-11-13 17:12:05 +0100

    Update logging.py

--- API/logging.py
+++ API/logging.py
@@ -4,7 +4,7 @@ import os
 import json
 import sys
 import cmd
-import constant
+from data.library.constant import *
 #format : [HH:MM:SS] [SOURCE/LEVEL]:content
 # %th %tm %ts %s %l %c
 def log(level, source, content):
