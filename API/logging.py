a85f12246456344387aa4120ecb05aeea73ea980
Armadamaindefeer <74682524+Armadamaindefeer@users.noreply.github.com>
2021-11-13 04:09:27 +0100

    Update logging.py

--- API/logging.py
+++ API/logging.py
@@ -5,6 +5,8 @@ import json
 import sys
 import cmd
 import constant
-
-
+#format : [HH:MM:SS] [SOURCE/LEVEL]:content
+# %th %tm %ts %s %l %c
 def log(level, source, content):
+
+	print('<placeholder>')
