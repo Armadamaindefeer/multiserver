463b69f7e7a8b405fa853ce8a01c98520f5390cb
Armadamaindefeer <74682524+Armadamaindefeer@users.noreply.github.com>
2021-11-20 01:25:24 +0100

    Update MultiServer.py

--- MultiServer.py
+++ MultiServer.py
@@ -9,17 +9,23 @@ import colorama
 import data.library.utility as utility
 import data.library.updater as updater
 from data.library.constant import *
+from data.library.logging import log
+
+SOURCE = __name__
 
 def init():
 	updater.checkLib()
 
 def main():
-	running = True
-
-	while running :
-		utility.cmdHandler()
-
-
+	try:
+		running = True
+		while running :
+			utility.cmdHandler()
+	except KeyboardInterrupt:
+		log(WARM, SOURCE, 'Soft interrupt : Do you want to exit ? [Y/n]')
+		if utility.userAnswerHandler(input(), True):
+			log(INFO, SOURCE, 'Stopping')
+			sys.exit()
 
 if __name__ == '__main__':
 	init()
